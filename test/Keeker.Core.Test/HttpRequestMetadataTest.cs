a2af6d43e4313542a654f6942ca7e37f8cafb74a
ak <ak@taucode.com>
2018-04-20 10:16:59 +0300

    added ut-s for request and response metadata

---
+++ test/Keeker.Core.Test/HttpRequestMetadataTest.cs
@@ -0,0 +1,121 @@
+﻿using NUnit.Framework;
+
+namespace Keeker.Core.Test
+{
+    [TestFixture]
+    public class HttpRequestMetadataTest
+    {
+        [Test]
+        public void Constructor_ValidArguments_RunsOk()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Constructor_LineIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Constructor_HeadersIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        [TestCase(@"жжжGET / HTTP/1.1
+Accept: text/html, application/xhtml+xml, image/jxr, */*
+Accept-Language: ru-RU
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299
+Accept-Encoding: gzip, deflate
+Host: allitebooks.com
+Connection: Keep-Alive
+
+")]
+        [TestCase(@"жжжGET / HTTP/1.1
+Accept: text/html, application/xhtml+xml, image/jxr, */*
+Accept-Language: ru-RU
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299
+Accept-Encoding: gzip, deflate
+Host: allitebooks.com
+Connection: Keep-Alive
+
+жжжAccept: text/html, application/xhtml+xml, image/jxr, */*
+Accept-Language: ru-RU
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299
+Accept-Encoding: gzip, deflate
+Host: allitebooks.com
+Connection: Keep-Alive
+
+")]
+        public void Parse_InputIsValid_ParsesCorrectly(string input)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Parse_BadLine_ThrowsBadHttpDataException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Parse_BadHeaders_ThrowsBadHttpDataException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Parse_OnlyOneCrLf_ThrowsBadHttpDataException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Parse_NoCrLfs_ThrowsBadHttpDataException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
---
+++ test/Keeker.Core.Test/HttpResponseMetadataTest.cs
@@ -0,0 +1,128 @@
+﻿using NUnit.Framework;
+
+namespace Keeker.Core.Test
+{
+    [TestFixture]
+    public class HttpResponseMetadataTest
+    {
+        [Test]
+        public void Constructor_ValidArguments_RunsOk()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Constructor_LineIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Constructor_HeadersIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        [TestCase(@"жжжHTTP/1.1 200 OK
+Server: nginx/1.8.0
+Date: Fri, 20 Apr 2018 07:15:11 GMT
+Content-Type: text/html; charset=UTF-8
+Transfer-Encoding: chunked
+Connection: keep-alive
+Vary: Accept-Encoding
+X-Powered-By: PHP/5.4.45
+X-Pingback: http://www.allitebooks.com/xmlrpc.php
+
+")]
+        [TestCase(@"HTTP/1.1 200 OK
+Server: nginx/1.8.0
+Date: Fri, 20 Apr 2018 07:15:11 GMT
+Content-Type: text/html; charset=UTF-8
+Transfer-Encoding: chunked
+Connection: keep-alive
+Vary: Accept-Encoding
+X-Powered-By: PHP/5.4.45
+X-Pingback: http://www.allitebooks.com/xmlrpc.php
+
+HTTP/1.1 200 OK
+Server: nginx/1.8.0
+Date: Fri, 20 Apr 2018 07:15:11 GMT
+Content-Type: text/html; charset=UTF-8
+Transfer-Encoding: chunked
+Connection: keep-alive
+Vary: Accept-Encoding
+X-Powered-By: PHP/5.4.45
+X-Pingback: http://www.allitebooks.com/xmlrpc.php
+
+")]
+        public void Parse_InputIsValid_ParsesCorrectly(string input)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Parse_BadLine_ThrowsBadHttpDataException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Parse_BadHeaders_ThrowsBadHttpDataException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Parse_OnlyOneCrLf_ThrowsBadHttpDataException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Parse_NoCrLfs_ThrowsBadHttpDataException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}

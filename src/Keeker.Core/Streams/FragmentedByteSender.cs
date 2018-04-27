42353d610e615f6d3a3efeeeb0dad388967d1ecc
ak <ak@taucode.com>
2018-04-27 12:42:54 +0300

    byte sender

---
+++ src/Keeker.Core/Streams/FragmentedByteSender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Keeker.Core.Streams
+{
+    public class FragmentedByteSender : IByteSender
+    {
+        private readonly Queue<ByteDelivery> _deliveries;
+
+        public FragmentedByteSender(Stream targetStream)
+        {
+            this.TargetStream = targetStream ?? throw new ArgumentNullException(nameof(targetStream));
+            _deliveries = new Queue<ByteDelivery>();
+        }
+
+        public void Send(byte[] buffer, int offset, int count)
+        {
+            var delivery = new ByteDelivery(buffer, offset, count);
+            _deliveries.Enqueue(delivery);
+        }
+
+        public Stream TargetStream { get; }
+
+        public void Release(int count)
+        {
+            var delivery = _deliveries.Peek();
+            var portion = delivery.Deliver(count);
+            this.TargetStream.Write(portion, 0, count);
+        }
+    }
+}
---
+++ src/Keeker.Core/Streams/TransparentByteSender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Keeker.Core.Streams
+{
+    public class TransparentByteSender : IByteSender
+    {
+        public TransparentByteSender(Stream targetStream)
+        {
+            this.TargetStream = targetStream ?? throw new ArgumentNullException(nameof(targetStream));
+        }
+
+        public void Send(byte[] buffer, int offset, int count)
+        {
+            this.TargetStream.Write(buffer, offset, count);
+        }
+
+        public Stream TargetStream { get; }
+    }
+}
